cebe9bdcaab58716be528d1660ecfec6d14e4376
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-10-04 10:42:42 +0530

    working on app navigation

---
+++ navigation/BookStackNavigator.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BookStackNavigator = () => {
+  return (
+    <View>
+      <Text>BookStackNavigator</Text>
+    </View>
+  )
+}
+
+export default BookStackNavigator
+
+const styles = StyleSheet.create({})
---
+++ navigation/PlayStackNavigator.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PlayStackNavigator = () => {
+  return (
+    <View>
+      <Text>PlayStackNavigator</Text>
+    </View>
+  )
+}
+
+export default PlayStackNavigator
+
+const styles = StyleSheet.create({})
