7fbdb9ba05cf346f0936cafaedefc7b435365cc4
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-10-10 12:16:36 +0530

    working...

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -11,15 +11,17 @@ const HomeScreen = () => {
      <SafeAreaView className='flex-1 bg-white'>
         <View className="px-4 py-3 bg-white flex-row justify-between items-center border-b border-gray-200">
           <View className="flex-1">
-            <Text>Location</Text>
-            <Text>Shakar Nagar, Bengaluru</Text>
+            <Text  className="text-gray-400 text-xs">Location</Text>
+            <Text className="text-lg font-semibold">Sahakar Nagar, Bengaluru</Text>
           </View>
 
           <View className="flex-row gap-4 items-center ml-2">
             <MessageCircle size={20} stroke="#333"/>
             <Bell  size={20} stroke="#333"/>
             <Pressable>
-               <Image source={{uri : "https://lh3.googleusercontent.com/ogw/AF2bZyg_eDli_setSnqEQAjrwfx8fiTlOoXO_MBekbrymm-xsw=s64-c-mo"}}/>
+               <Image 
+                className="w-8 h-8 rounded-full"
+                source={{uri : "https://lh3.googleusercontent.com/ogw/AF2bZyg_eDli_setSnqEQAjrwfx8fiTlOoXO_MBekbrymm-xsw=s64-c-mo"}}/>
             </Pressable>
           </View>
         </View>
