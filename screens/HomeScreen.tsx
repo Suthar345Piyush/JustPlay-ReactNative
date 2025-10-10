c94c0bc7bbc4cdb4c91fec477221f5ffba5c5e38
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-10-10 15:44:20 +0530

    home screen work

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { Image,Pressable,Text,View} from 'react-native'
+import { Image,Pressable,ScrollView,Text,TouchableOpacity,View} from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -12,7 +12,7 @@ const HomeScreen = () => {
         <View className="px-4 py-3 bg-white flex-row justify-between items-center border-b border-gray-200">
           <View className="flex-1">
             <Text  className="text-gray-400 text-xs">Location</Text>
-            <Text className="text-lg font-semibold">Sahakar Nagar, Bengaluru</Text>
+            <Text className="text-lg font-semibold text-black">Sahakar Nagar, Bengaluru</Text>
           </View>
 
           <View className="flex-row gap-4 items-center ml-2">
@@ -25,6 +25,38 @@ const HomeScreen = () => {
             </Pressable>
           </View>
         </View>
+
+        <ScrollView className="px-4" showsVerticalScrollIndicator={false}>
+          <View className="bg-[#F4F4F5] rounded-2xl p-4 mt-4 flex-row items-center justify-between">
+            <View className="">
+              <Text className="text-lg font-semibold">Set your Weekly Fit Goal🔥</Text>
+              <Text className="text-sm text-gray-500 mt-1">KEEP YOURSELF FIT!</Text>
+            </View>
+            <Text className="text-3xl">➡️</Text>
+          </View>
+
+          <View>
+            <Text>START PLAYING!</Text>
+            <View>
+              <Text>
+                Create Game
+              </Text>
+              <Text>
+                No upcoming games in your calendar
+              </Text>
+            </View>
+            <TouchableOpacity>
+              <Text>
+                Create
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity>
+            <Text>
+              View My Calendar
+            </Text>
+          </TouchableOpacity>
+        </ScrollView>
      </SafeAreaView>
   )
 }
